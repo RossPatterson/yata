b417edbf48f8505ae3beba514e86bcd14978881f
Ross Patterson <ross.patterson@gmail.com>
2025-02-02 17:49:31 -0500

    Run tests.

--- test/runtest.sh
+++ test/runtest.sh
@@ -2,38 +2,34 @@ let exit_rc=0
 
 echo Test 1: Create and verify an archive
 rm archive.yata 2>/dev/null
+let my_rc=0
 ./yata -c -d ./in_data
+let my_rc=$my_rc+$?
+diff -u in_data/archive.yata archive.yata
 let rc=$?
-if [[ $rc != 0 ]] ; then echo Test failed
+if [[ $my_rc != 0 ]] ; then echo Test failed
+else echo Test passed
 fi
-let exit_rc=$exit_rc+$rc
-cmp in_data/archive.yata archive.yata
-let rc=$?
-if [[ $rc != 0 ]] ; then echo Test failed
-fi
-let exit_rc=$exit_rc+$rc
+let exit_rc=$exit_rc+$my_rc
 rm archive.yata 2>/dev/null
 
 echo Test 2: Extract and verify an archive
 rm -rf out_data 2>/dev/null
 mkdir out_data
+let my_rc=0
 ./yata -x -f in_data/archive.yata -d ./out_data
-let rc=$?
-if [[ $rc != 0 ]] ; then echo Test failed
-fi
-let exit_rc=$exit_rc+$rc
+let my_rc=$my_rc+$?
 files=$(ls in_data/*.exec out_data/*.exec)
-if [[ $rc != 0 ]] ; then echo Test failed
-fi
-let exit_rc=$exit_rc+$rc
+let my_rc=$my_rc+$?
 for f in $files ; do
 	f=`basename $f`
-	cmp in_data/$f out_data/$f
-	let rc=$?
-	if [[ $rc != 0 ]] ; then echo Test failed
-	fi
-	let exit_rc=$exit_rc+$rc
+	diff -u in_data/$f out_data/$f
+	let my_rc=$my_rc+$?
 done
+if [[ $my_rc != 0 ]] ; then echo Test failed
+else echo Test passed
+fi
+let exit_rc=$exit_rc+$my_rc
 rm -rf out_data 2>/dev/null
 
 exit $exit_rc
