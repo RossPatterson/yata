03cd90d67c17673d143e6951533981825725f050
Ross Patterson <ross.patterson@gmail.com>
2025-02-03 08:39:18 -0500

    Clean up Windows tests.

--- test/runtest.bat
+++ test/runtest.bat
@@ -1,30 +1,35 @@
 @SETLOCAL
 @ECHO OFF
 
-SET RC=0
+SET EXIT_RC=0
 
 ECHO Test 1: Create and verify an archive
 DEL archive.yata 2>NUL:
+SET MY_RC=0
 yata -c -d .\in_data
-IF %ERRORLEVEL% NEQ 0 ECHO Test failed - yata RC=%ERRORLEVEL%
-SET /A RC=%RC%+%ERRORLEVEL%
+SET /A MY_RC=%MY_RC%+%ERRORLEVEL%
 CALL :COMPARE_FILE in_data\archive.yata archive.yata
+IF %MY_RC% NEQ 0 ECHO Test failed
+IF %MY_RC% EQU 0 ECHO Test passed
+SET /A EXIT_RC=%EXIT_RC%+%MY_RC%
 DEL archive.yata 2>NUL:
 
 ECHO Test 2: Extract and verify an archive
 RMDIR /Q/S out_data 2>NUL:
 MKDIR out_data
+SET MY_RC=0
 yata -x -f in_data\archive.yata -d .\out_data
-IF %ERRORLEVEL% NEQ 0 ECHO Test failed - yata RC=%ERRORLEVEL%
-SET /A RC=%RC%+%ERRORLEVEL%
+SET /A MY_RC=%MY_RC%+%ERRORLEVEL%
 FOR %%F in (in_data\*.exec) DO CALL :COMPARE_FILE "in_data\%%~nxF" "out_data\%%~nxF"
 FOR %%F in (out_data\*.exec) DO CALL :COMPARE_FILE "in_data\%%~nxF" "out_data\%%~nxF"
 RMDIR /Q/S out_data 2>NUL:
+IF %MY_RC% NEQ 0 ECHO Test failed
+IF %MY_RC% EQU 0 ECHO Test passed
+SET /A EXIT_RC=%EXIT_RC%+%MY_RC%
 
-EXIT /B %RC%
+EXIT /B %EXIT_RC%
 
 :COMPARE_FILE
 comp /m /a "%~1" "%~2"
-IF %ERRORLEVEL% NEQ 0 ECHO Test failed - files differ
-SET /A RC=%RC%+%ERRORLEVEL%
+SET /A MY_RC=%MY_RC%+%ERRORLEVEL%
 GOTO :EOF
